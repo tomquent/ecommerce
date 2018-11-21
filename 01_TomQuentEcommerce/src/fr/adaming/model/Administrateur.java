1b5d85547c2a408df64f63786daf781b9056de54
Adaming <Adaming@DESKTOP-UB6U9PU>
2018-11-21 18:42:03 +0100

    Thomas : ajout de la classe Administrateur + énumération des classes persistente dans le fichier de configuration persistence.xml

---
+++ 01_TomQuentEcommerce/src/fr/adaming/model/Administrateur.java
@@ -0,0 +1,14 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.Entity;
+
+@Entity
+@RequestScoped
+public class Administrateur implements Serializable {
+
+	
+	
+}
